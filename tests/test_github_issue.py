c401e9a325f1b3937fa4c42f6421fa097f8650a1
saveleva_a <saveleva_a@tochka.com>
2022-07-20 15:14:43 +0100

    refactoring

--- tests/test_github_issue.py
+++ tests/test_github_issue.py
@@ -5,19 +5,27 @@ from selene.support.shared import browser
 from selene.support.shared.jquery_style import s
 import allure
 
+repository = "eroshenkoam/allure-example"
+issue = "#76"
+
 
 def test_github_selene():
     browser.open("https://github.com")
 
-    s(".header-search-input").click()
-    s(".header-search-input").send_keys("eroshenkoam/allure-example")
-    s(".header-search-input").submit()
+    (
+        s(".header-search-input")
+        .click()
+        .send_keys(repository)
+        .submit()
+     )
 
-    s(by.link_text("eroshenkoam/allure-example")).click()
+
+    s(by.link_text(repository)).click()
 
     s("#issues-tab").click()
 
-    s(by.partial_text("#76")).should(be.visible)
+    s(by.partial_text(issue)).should(be.visible)
+
 
 @allure.tag("GitHub")
 @allure.severity(Severity.NORMAL)
@@ -29,34 +37,40 @@ def test_steps_dynamic():
     with allure.step("Open page github.com"):
         browser.open("https://github.com")
 
-    with allure.step("Find repository 'eroshenkoam/allure-example'"):
-        s(".header-search-input").click()
-        s(".header-search-input").send_keys("eroshenkoam/allure-example")
-        s(".header-search-input").submit()
+    with allure.step("Find repository '{repository}'"):
+        (
+            s(".header-search-input")
+            .click()
+            .send_keys(repository)
+            .submit()
+        )
 
     with allure.step("Go to repository"):
-        s(by.link_text("eroshenkoam/allure-example")).click()
+        s(by.link_text(repository)).click()
         s("#issues-tab").click()
 
-    with allure.step("Check Issue #76"):
-        s(by.partial_text("#76")).should(be.visible)
+    with allure.step("Check Issue {issue}"):
+        s(by.partial_text(issue)).should(be.visible)
+
 
 def test_steps_decorate():
     open_page("https://github.com")
-    find_repository("eroshenkoam/allure-example")
-    go_to_repository("eroshenkoam/allure-example")
-    search_issue_number("#76")
+    find_repository(repository)
+    go_to_repository(repository)
+    search_issue_number(issue)
 
 
 @allure.step('Зайти на страницу {url}')
 def open_page(url: str):
     browser.open(url)
 
+
 @allure.step('Найти в поиске репозиторий {repository_name}')
 def find_repository(repository_name: str):
     s(".header-search-input").click()
     s(".header-search-input").send_keys(repository_name)
     s(".header-search-input").submit()
+
 
 @allure.step('Перейти в репозиторий {repository_name}')
 def go_to_repository(repository_name: str):
