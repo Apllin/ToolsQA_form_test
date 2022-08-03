fac51b6ed1d99c9f98d2de2883e226ec1ed7e3f6
saveleva_a <saveleva_a@tochka.com>
2022-08-03 18:36:01 +0100

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,18 @@
+import os
+
 from selene.support.shared import browser
 import pytest
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-
+from dotenv import load_dotenv
 from utils import attach
 
 DEFAULT_BROWSER_VERSION = '100.0'
+
+
+@pytest.fixture(scope="session", autouse=True)
+def load_env():
+    load_dotenv()
 
 
 def pytest_addoption(parser):
@@ -48,8 +55,11 @@ def setup_browser(request):
 
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv("LOGIN")
+    password = os.getenv("PASSWORD")
+
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options)
 
     # browser = Browser(Config(driver))
